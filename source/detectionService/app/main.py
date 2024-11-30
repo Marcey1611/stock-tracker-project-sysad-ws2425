7a0551677f69b74d1531edcc3f825e361f34b669
mw-224023 <manuel.walser@rwu.de>
2024-11-30 15:55:19 +0100

    all tests are good, (addItem, deleteItem, NextID(only with a fix int))

--- source/detectionService/app/main.py
+++ source/detectionService/app/main.py
@@ -4,8 +4,8 @@ import requests
 
 app = FastAPI()
 
-# TODO: wieder entfernen sobald test positiv
-
+# TODO: wieder entfernen sobald testreihe positiv
+'''
 @app.get("/addItems")
 def testRequestAddItems():
     payload = {
@@ -13,12 +13,48 @@ def testRequestAddItems():
         "classID": 5,
         "SystemInTime": "2024-11-30 13:00:00",
     }
-    
     try:
-        response = requests.post("http://database-service:8000/add_item", json=payload)
-        
+        response = requests.post("http://database-service:8000/add_item", json=payload) 
         return {"response": response.json()}
-    
-    except requests.exceptions.RequestException as exception:
-        
+    except requests.exceptions.RequestException as exception:  
         return {"status": "error", "message": str(exception)}
+'''
+'''         
+#zweiter Datensatz
+@app.get("/addItems")
+def testRequestAddItems():
+    payload = {
+        "id": 7,
+        "classID": 2,
+        "SystemInTime": "2024-11-30 14:40:00",
+    }
+    try:
+        response = requests.post("http://database-service:8000/add_item", json=payload)  
+        return {"response": response.json()}
+    except requests.exceptions.RequestException as exception:  
+        return {"status": "error", "message": str(exception)}
+'''
+'''
+@app.get("/deleteItems")
+def testRequestDeleteItems():
+    payload = {
+        "id": 3,
+        "classID": 5,
+        "SystemOutTime": "2024-11-30 14:45:00",
+    }
+    try:
+        response = requests.post("http://database-service:8000/delete_item", json=payload)  
+        return {"response": response.json()}
+    except requests.exceptions.RequestException as exception:  
+        return {"status": "error", "message": str(exception)}
+''' 
+
+@app.get("/getNextID")
+
+def testRequestGetNextID():
+    try:
+        response = requests.get("http://database-service:8000/get_next_id")
+        return {"response": response.json()}
+    except requests.exceptions.RequestException as exception:  
+        return {"status": "error", "message": str(exception)}
+
