d01b839cbd525498c0ffc187aadb46bb9d3f7438
Marcel <marcel.eichelberger@t-online.de>
2024-12-07 11:00:23 +0100

    SYSAD-18: improved code of api.py

--- source/mailingService/app/api/boundary/api.py
+++ source/mailingService/app/api/boundary/api.py
@@ -5,30 +5,26 @@ from api.validation.validator import Validator
 from entity.enums import Action
 
 router = APIRouter()
+apiBF = ApiBF()
+validator = Validator()
 
 @router.post("/sendMailAdded")
 async def sendMailAddedPostInterface(request: Request):
     requestData = await request.json()
-    validator = Validator()
     validData = validator.validateData(requestData)
-    apiBF = ApiBF()
     return await apiBF.prepareMailingData(validData, Action.ADDED)
 
 @router.post("/sendMailDeleted")
 async def sendMailDeletedPostInterface(request: Request):
     requestData = await request.json()
-    validator = Validator()
     validData = validator.validateData(requestData)
-    apiBF = ApiBF()
     return await apiBF.prepareMailingData(validData, Action.DELETED)
 
 @router.post("/sendErrorMail")
 async def sendErrorMailPostInterface(request: Request):
     requestData = await request.json()
-    validator = Validator()
     validator.validateErrorMessage(requestData)
     validData = validator.validateData(requestData)
-    apiBF = ApiBF()
     return await apiBF.prepareMailingData(validData, Action.ERROR)
 
 
