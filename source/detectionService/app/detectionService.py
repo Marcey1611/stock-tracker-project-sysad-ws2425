636784a3a923465e48a51f7bfe1afdde53789fff
np-223996 <np-223996@rwu.de>
2024-12-07 10:39:10 +0100

    #SYSAD-37 resolved MR comment

--- source/detectionService/app/detectionService.py
+++ source/detectionService/app/detectionService.py
@@ -6,112 +6,105 @@ import numpy as np
 
 from databaseService import manageAddToDatabase, manageDeleteToDatabase
 from RequestModuls import AddRequest
-
+from source.detectionService.app.detectionModules import TrackerManager
 
 # Load the YOLO11 model
 model = YOLO("best.pt")
 
-def yolo_detection():
-    track_history = defaultdict(lambda: [])
-    stay_time = defaultdict(lambda: 0)  # Verweilzeit für jedes Objekt
-    detected_objects = set()  # Objekte, die sich im Bild befinden
-    previous_detected_objects = set()  # Zustand des vorherigen Frames
-    disappearance_time = defaultdict(lambda: 0)  # Zeit, wie lange ein Objekt verschwunden ist
-    cls_id_history = defaultdict(lambda: defaultdict(lambda: 0))  # Speichert die cls_id-Häufigkeit pro track_id
-    rest_models = defaultdict(lambda: AddRequest(None,None,None))
+def process_frame(frame):
+    results = model.track(frame, conf=0.4, persist=True, verbose=False, imgsz=640)
+    annotated_frame = results[0].plot()
+    return results, annotated_frame
 
-    TOLERANCE = 10  # Positionstoleranz in Pixeln
-    ADD_THRESHOLD = 1 * 30  # Delay beim Hinzufügen
-    REMOVE_THRESHOLD = 1 * 30  # Delay beim Entfernen
+def update_object_tracking(results, trackers, TOLERANCE, ADD_THRESHOLD):
+    boxes = results[0].boxes.xywh.cpu()
+    track_ids = results[0].boxes.id.int().cpu().tolist()
+    cls_ids = results[0].boxes.cls.int().cpu().tolist()
+
+    current_track_ids = set()
+    for box, track_id, cls_id in zip(boxes, track_ids, cls_ids):
+        x, y, w, h = map(float, box)
+        current_track_ids.add(track_id)
+
+        # Aktualisiere Klassenhistorie
+        trackers.cls_id_history[track_id][cls_id] += 1
+
+        # Berechne Entfernung und aktualisiere Verweilzeit
+        if trackers.track_history[track_id]:
+            last_x, last_y = trackers.track_history[track_id][-1]
+            distance = ((x - last_x) ** 2 + (y - last_y) ** 2) ** 0.5
+        else:
+            distance = float('inf')
+
+        if distance <= TOLERANCE:
+            trackers.stay_time[track_id] += 1
+        else:
+            trackers.stay_time[track_id] = 0
+
+        # Objekt hinzufügen, wenn es stabil bleibt
+        if trackers.stay_time[track_id] >= ADD_THRESHOLD:
+            trackers.detected_objects.add(track_id)
+            trackers.disappearance_time[track_id] = 0
+
+        # Speichere Positionshistorie
+        trackers.track_history[track_id].append((x, y))
+        if len(trackers.track_history[track_id]) > 30:
+            trackers.track_history[track_id].pop(0)
+    return current_track_ids
+
+def handle_disappeared_objects(current_track_ids, trackers, REMOVE_THRESHOLD):
+    for track_id in trackers.detected_objects.copy():
+        if track_id not in current_track_ids:
+            trackers['disappearance_time'][track_id] += 1
+            if trackers['disappearance_time'][track_id] >= REMOVE_THRESHOLD:
+                trackers.detected_objects.remove(track_id)
+        else:
+            trackers['disappearance_time'][track_id] = 0
+
+def update_database(added_objects, removed_objects, trackers):
+    for added_object in added_objects:
+        if trackers.cls_id_history[added_object]:
+            most_frequent_cls_id = max(
+                trackers.cls_id_history[added_object],
+                key=trackers.cls_id_history[added_object].get
+            )
+            manageAddToDatabase(trackers.rest_models, added_object, most_frequent_cls_id)
+
+    for removed_object in removed_objects:
+        manageDeleteToDatabase(trackers.rest_models, removed_object)
+
+def yolo_detection():
+    TOLERANCE = 10
+    ADD_THRESHOLD = 1 * 30
+    REMOVE_THRESHOLD = 1 * 30
+
+    trackers = TrackerManager()
 
     while camera.isOpened():
         success, frame = camera.read()
-
-        if success:
-            annotated_frame = frame.copy()
-
-            results = model.track(frame,conf=0.4,persist=True, verbose=False,imgsz=640)
-            current_track_ids = set()
-
-            # Wenn es erkannte Boxen gibt
-            if results[0].boxes is not None and results[0].boxes.id is not None:
-                boxes = results[0].boxes.xywh.cpu()
-                track_ids = results[0].boxes.id.int().cpu().tolist()
-                cls_ids = results[0].boxes.cls.int().cpu().tolist()  # Klassen-IDs
-
-                annotated_frame = results[0].plot()
-
-                for box, track_id, cls_id in zip(boxes, track_ids, cls_ids):
-                    x, y, w, h = box
-                    x, y = float(x), float(y)
-
-                    current_track_ids.add(track_id)
-
-                    # Aktualisiere Klassenhistorie
-                    cls_id_history[track_id][cls_id] += 1
-
-                    # Berechne Entfernung zur letzten bekannten Position
-                    if track_history[track_id]:
-                        last_x, last_y = track_history[track_id][-1]
-                        distance = ((x - last_x) ** 2 + (y - last_y) ** 2) ** 0.5
-                    else:
-                        distance = float('inf')
-
-                    # Aktualisiere Verweilzeit
-                    if distance <= TOLERANCE:
-                        stay_time[track_id] += 1
-                    else:
-                        stay_time[track_id] = 0
-
-                    # Wenn Objekt hinzugefügt wird
-                    if stay_time[track_id] >= ADD_THRESHOLD:
-                        detected_objects.add(track_id)
-                        disappearance_time[track_id] = 0
-
-                    # Speichere Positionshistorie
-                    track_history[track_id].append((x, y))
-                    if len(track_history[track_id]) > 30:
-                        track_history[track_id].pop(0)
-
-                    # Zeichne Bewegungsverlauf
-                    points = np.hstack(track_history[track_id]).astype(np.int32).reshape((-1, 1, 2))
-                    cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
-
-            # Prüfe auf verschwundene Objekte
-            for track_id in detected_objects.copy():
-                if track_id not in current_track_ids:
-                    disappearance_time[track_id] += 1
-                    if disappearance_time[track_id] >= REMOVE_THRESHOLD:
-                        detected_objects.remove(track_id)
-                else:
-                    disappearance_time[track_id] = 0
-
-            # Hinzugefügte und entfernte Objekte feststellen
-            added_objects = detected_objects - previous_detected_objects
-            removed_objects = previous_detected_objects - detected_objects
-            if added_objects:
-                for added_object in added_objects:
-                    if cls_id_history[added_object]:
-                        most_frequent_cls_id = max(cls_id_history[added_object], key=cls_id_history[added_object].get)
-                        manageAddToDatabase(rest_models,added_object,most_frequent_cls_id)
-
-            if removed_objects:
-                for removed_object in removed_objects:
-                    manageDeleteToDatabase(rest_models,removed_object)
-
-            previous_detected_objects = detected_objects.copy()
-
-            # Frame kodieren und zurückgeben
-            ret, buffer = cv2.imencode('.jpg', annotated_frame)
-            if not ret:
-                break
-
-            frame_bytes = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
-        else:
+        if not success:
             break
+
+        results, annotated_frame = process_frame(frame)
+
+        if results[0].boxes is not None and results[0].boxes.id is not None:
+            current_track_ids = update_object_tracking(results, trackers, TOLERANCE, ADD_THRESHOLD)
+            handle_disappeared_objects(current_track_ids, trackers, REMOVE_THRESHOLD)
+
+            added_objects = trackers.detected_objects - trackers.previous_detected_objects
+            removed_objects = trackers.previous_detected_objects - trackers.detected_objects
+            update_database(added_objects, removed_objects, trackers)
+
+            trackers.previous_detected_objects = trackers.detected_objects.copy()
+
+        # Frame kodieren und zurückgeben
+        ret, buffer = cv2.imencode('.jpg', annotated_frame)
+        if not ret:
+            break
+
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
 
     camera.release()
     cv2.destroyAllWindows()
-
