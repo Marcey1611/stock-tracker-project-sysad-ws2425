866d69a77adb72b3203067ae6ed74413286ad8c7
np-223996 <np-223996@rwu.de>
2024-12-15 12:45:15 +0100

    #SYSAD-43 MR resolved 3

--- source/detectionService/app/service/detection/detectionThread.py
+++ source/detectionService/app/service/detection/detectionThread.py
@@ -1,132 +1,65 @@
 import logging
 import queue
 from threading import Event
-from ultralytics import YOLO
 import cv2
-import time
 import api.apiRestClientDatabase as ApiRestClientDatabase
+
+from service.detection.frameProccess import processFrame
 
 logger = logging.getLogger('detectionThread')
 
-model = YOLO("./service/detection/best.pt")
 
 def detectionThread(feedEvent:Event,feedQ:queue.Queue,trackEvent:Event,trackQ:queue.Queue,source):
-    try:
-        ApiRestClientDatabase.clearAll()
-        camera = cv2.VideoCapture(source)
-        if camera.isOpened():
-            logger.info(f"Camera {source} opend.")
-        else:
-            logger.error(f"Could not open camera:{source}.")
-            return  # Beende den Thread, wenn die Kamera nicht geöffnet werden kann
-        initCam(camera,source)
+    ApiRestClientDatabase.clearAll()
+    camera = cv2.VideoCapture(source)
+    if camera.isOpened():
+        logger.info(f"Camera {source} opend.")
+    else:
+        logger.error(f"Could not open camera:{source}.")
+        return  # Beende den Thread, wenn die Kamera nicht geöffnet werden kann
+    initCam(camera,source)
 
-        while True:
-            success, frame = camera.read()  # Frame von der Kamera lesen
+    while True:
+        success, frame = camera.read()  # Frame von der Kamera lesen
+        if not success:
+            logger.error("Error could not access camera frame.")
+            break
+        else:
+            success, frame = camera.read()
             if not success:
-                logger.error("Error could not access camera frame.")
                 break
-            else:
-                success, frame = camera.read()
-                if not success:
+
+            annotatedFrame = processFrame(frame)
+
+            if feedEvent.is_set():
+                streamFeedFrames(frame, feedQ)
+            if trackEvent.is_set():
+                if not streamTrackFrames(annotatedFrame, trackQ):
                     break
 
-                annotatedFrame = processFrame(frame)
+    camera.release()
+    logger.info(f"Camera {source} closed.")
 
-                if feedEvent.is_set():
-                    _, buffer = cv2.imencode('.jpg', frame)
-                    frameBytes = buffer.tobytes()
-                    feedQ.put(b'--frame\r\n'
-                            b'Content-Type: image/jpeg\r\n\r\n' + frameBytes + b'\r\n')
-                if trackEvent.is_set():
-                    ret, buffer = cv2.imencode('.jpg', annotatedFrame)
-                    if not ret:
-                        break
+def streamFeedFrames(frame,feedQ:queue.Queue):
+    _, buffer = cv2.imencode('.jpg', frame)
+    frameBytes = buffer.tobytes()
+    feedQ.put(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frameBytes + b'\r\n')
 
-                    frameBytes = buffer.tobytes()
-                    trackQ.put(b'--frame\r\n'
-                            b'Content-Type: image/jpeg\r\n\r\n' + frameBytes + b'\r\n')
+def streamTrackFrames(annotatedFrame,trackQ):
+    ret, buffer = cv2.imencode('.jpg', annotatedFrame)
+    if not ret:
+        return False
 
-        camera.release()
-        logger.info(f"Camera {source} closed.")
-    except cv2.error as e:
-        logger.error(f"OpenCV-Error: {e}")
+    frameBytes = buffer.tobytes()
+    trackQ.put(b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frameBytes + b'\r\n')
+    return True
 
 
 
-objectCountHistory = {}  # Historie der erkannten Objekte pro Klasse
-objectTimestamps = {}  # Zeitstempel für das erste Auftreten jeder Klasse
-removalTimestamps = {}  # Zeitstempel für das letzte Verschwinden jeder Klasse
-DELAY = 2  # Verzögerung in Sekunden, bevor ein Objekt als hinzugefügt/entfernt gilt
 
-def processFrame(frame):
-    results = model.predict(frame, conf=0.55, imgsz=640, verbose=False)
-    annotatedFrame = results[0].plot()
-    detected_objects = results[0].boxes.cls.int().cpu().tolist()
-    currentClasses = set(detected_objects)
 
-    addedClasses = []
-    removedClasses = []
-
-    # Initialisiere die Historie beim ersten Aufruf
-    global objectCountHistory, objectTimestamps, removalTimestamps
-    current_time = time.time()
-
-    if not objectCountHistory:
-        objectCountHistory = {cls: True for cls in currentClasses}
-        objectTimestamps = {cls: current_time for cls in currentClasses}
-        addedClasses = list(currentClasses)
-        updateDatabase(addedClasses, removedClasses)
-        return annotatedFrame
-
-    checkForAddObjects(currentClasses,current_time, addedClasses)
-
-    checkForDelObjects(currentClasses, current_time, removedClasses)
-
-    # Aktualisiere Zeitstempel für erkannte Objekte
-    for cls in list(objectTimestamps.keys()):
-        if cls not in currentClasses:
-            del objectTimestamps[cls]  # Entferne Zeitstempel, wenn die Klasse nicht mehr erkannt wird
-
-    for cls in list(removalTimestamps.keys()):
-        if cls in currentClasses:
-            del removalTimestamps[cls]  # Entferne den Entfernungstempel, wenn die Klasse wieder erkannt wird
-
-    updateDatabase(addedClasses, removedClasses)
-    return annotatedFrame
-def checkForAddObjects(currentClasses,current_time,addedClasses):
-    # Überprüfe hinzugefügte Klassen
-    for cls in currentClasses:
-        if cls not in objectCountHistory:
-            if cls not in objectTimestamps:
-                objectTimestamps[cls] = current_time  # Speichere den Zeitstempel
-            elif current_time - objectTimestamps[cls] >= DELAY:
-                addedClasses.append(cls)
-                objectCountHistory[cls] = True
-                del objectTimestamps[cls]  # Entferne den Zeitstempel, da die Klasse nun hinzugefügt wurde
-
-def checkForDelObjects(currentClasses,current_time,removedClasses):
-    for cls in list(objectCountHistory.keys()):
-        if cls not in currentClasses:
-            if cls not in removalTimestamps:
-                removalTimestamps[cls] = current_time  # Speichere den Zeitstempel
-            elif current_time - removalTimestamps[cls] >= DELAY:
-                removedClasses.append(cls)
-                del objectCountHistory[cls]
-                del removalTimestamps[cls]  # Entferne den Zeitstempel, da die Klasse nun entfernt wurde
-
-def updateDatabase(addedObjects, removedObjects):
-    if len(addedObjects):
-        addArray=[]
-        for addedObject in addedObjects:
-            addArray.append(addedObjects)
-        ApiRestClientDatabase.addItemToDatabase(addArray)
-
-    if len(removedObjects)>0:
-        removeArray = []
-        for removedObject in removedObjects:
-            removeArray.append(removedObject)
-        ApiRestClientDatabase.deleteItemFromDatabase(removeArray)
 
 def initCam(camera:cv2.VideoCapture,source):
     desiredWidth = 1920
---
+++ source/detectionService/app/service/detection/frameProccess.py
@@ -0,0 +1,81 @@
+from ultralytics import YOLO
+import time
+import api.apiRestClientDatabase as ApiRestClientDatabase
+
+objectCountHistory = {}  # Historie der erkannten Objekte pro Klasse
+objectTimestamps = {}  # Zeitstempel für das erste Auftreten jeder Klasse
+removalTimestamps = {}  # Zeitstempel für das letzte Verschwinden jeder Klasse
+DELAY = 2  # Verzögerung in Sekunden, bevor ein Objekt als hinzugefügt/entfernt gilt
+
+
+model = YOLO("./service/detection/best.pt")
+
+
+def processFrame(frame):
+    results = model.predict(frame, conf=0.55, imgsz=640, verbose=False)
+    annotatedFrame = results[0].plot()
+    detected_objects = results[0].boxes.cls.int().cpu().tolist()
+    currentClasses = set(detected_objects)
+
+    addedClasses = []
+    removedClasses = []
+
+    # Initialisiere die Historie beim ersten Aufruf
+    global objectCountHistory, objectTimestamps, removalTimestamps
+    current_time = time.time()
+
+    if not objectCountHistory:
+        objectCountHistory = {cls: True for cls in currentClasses}
+        objectTimestamps = {cls: current_time for cls in currentClasses}
+        addedClasses = list(currentClasses)
+        updateDatabase(addedClasses, removedClasses)
+        return annotatedFrame
+
+    checkForAddObjects(currentClasses,current_time, addedClasses)
+
+    checkForDelObjects(currentClasses, current_time, removedClasses)
+
+    # Aktualisiere Zeitstempel für erkannte Objekte
+    for cls in list(objectTimestamps.keys()):
+        if cls not in currentClasses:
+            del objectTimestamps[cls]  # Entferne Zeitstempel, wenn die Klasse nicht mehr erkannt wird
+
+    for cls in list(removalTimestamps.keys()):
+        if cls in currentClasses:
+            del removalTimestamps[cls]  # Entferne den Entfernungstempel, wenn die Klasse wieder erkannt wird
+
+    updateDatabase(addedClasses, removedClasses)
+    return annotatedFrame
+def checkForAddObjects(currentClasses,current_time,addedClasses):
+    # Überprüfe hinzugefügte Klassen
+    for cls in currentClasses:
+        if cls not in objectCountHistory:
+            if cls not in objectTimestamps:
+                objectTimestamps[cls] = current_time  # Speichere den Zeitstempel
+            elif current_time - objectTimestamps[cls] >= DELAY:
+                addedClasses.append(cls)
+                objectCountHistory[cls] = True
+                del objectTimestamps[cls]  # Entferne den Zeitstempel, da die Klasse nun hinzugefügt wurde
+
+def checkForDelObjects(currentClasses,current_time,removedClasses):
+    for cls in list(objectCountHistory.keys()):
+        if cls not in currentClasses:
+            if cls not in removalTimestamps:
+                removalTimestamps[cls] = current_time  # Speichere den Zeitstempel
+            elif current_time - removalTimestamps[cls] >= DELAY:
+                removedClasses.append(cls)
+                del objectCountHistory[cls]
+                del removalTimestamps[cls]  # Entferne den Zeitstempel, da die Klasse nun entfernt wurde
+
+def updateDatabase(addedObjects, removedObjects):
+    if len(addedObjects):
+        addArray=[]
+        for addedObject in addedObjects:
+            addArray.append(addedObjects)
+        ApiRestClientDatabase.addItemToDatabase(addArray)
+
+    if len(removedObjects)>0:
+        removeArray = []
+        for removedObject in removedObjects:
+            removeArray.append(removedObject)
+        ApiRestClientDatabase.deleteItemFromDatabase(removeArray)
