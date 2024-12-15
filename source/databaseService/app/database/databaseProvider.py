f89889d8489e5e2567df56bb210346e8133187e1
MikeLach22 <mikelachmuth@gmail.com>
2024-12-15 12:49:24 +0100

    Changed Vairables

--- source/databaseService/app/database/databaseProvider.py
+++ source/databaseService/app/database/databaseProvider.py
@@ -1,14 +1,17 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 class DatabaseProvider:
-    DATABASE_URL = "sqlite:///./test.db"
     Base = declarative_base()
 
     def __init__(self):
+        load_dotenv()
+
         # Initilies the engine and the session fabricator
-        self.engine = create_engine(self.DATABASE_URL)
+        self.engine = create_engine(os.getenv("DATABASE_URL"))
         self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)  
 
     def initDb(self):
