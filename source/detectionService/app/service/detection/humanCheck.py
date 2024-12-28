f949f9f07285826a45dabb5500229dba66b83b9c
np-223996 <np-223996@rwu.de>
2024-12-28 09:56:33 +0100

    #SYSAD-42 update

--- source/detectionService/app/service/detection/humanCheck.py
+++ source/detectionService/app/service/detection/humanCheck.py
@@ -8,8 +8,8 @@ logger = logging.getLogger(__name__)
 
 def isHumanInFrame(frame):
     results = model.predict(frame, conf=0.55, imgsz=640, verbose=False)
-    filtered_boxes = results[0].boxes[results[0].boxes.cls == 0]
-    results[0].boxes = filtered_boxes
+    filteredBoxes = results[0].boxes[results[0].boxes.cls == 0]
+    results[0].boxes = filteredBoxes
     annotated_frame = results[0].plot()
     classes = results[0].boxes.cls.cpu().numpy()
     if 0 in classes:
