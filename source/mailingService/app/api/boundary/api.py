f251ce7ac003e5cf40003bc52ef4275836964328
me-223965 <me-223965@rwu.de>
2025-02-12 09:11:13 +0100

    SYSAD-87: removed now unnecessary classes from mailing service

--- source/mailingService/app/api/boundary/api.py
+++ source/mailingService/app/api/boundary/api.py
@@ -2,13 +2,11 @@ import logging
 from fastapi import APIRouter, Request
 
 from api.control.api_bf import ApiBf
-from api.validation.validator import Validator
 from entity.models.mail_data import MailUpdateData, MailErrorData
 from entity.enums.action import Action
 
 api_router = APIRouter()
 api_bf = ApiBf()
-validator = Validator()
 
 @api_router.post("/send_update_mail")
 async def send_update_mail(request: list[MailUpdateData]):
