fc4d814eeb469635cef24c97b6c026a4afe112e3
np-223996 <np-223996@rwu.de>
2025-02-19 11:08:31 +0100

    #SYSAD-83 changed ports of stocktrackerapp 3

--- source/detectionService/app/service/detection/frame_drawings.py
+++ source/detectionService/app/service/detection/frame_drawings.py
@@ -2,6 +2,6 @@ import cv2
 
 def draw_bounding_box(frame, detected_object):
     x_min, y_min, x_max, y_max = detected_object.get_bounding_box()
-    cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255, 0, 0), 2)
+    cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (216,235,49), 4)
 
     return frame
