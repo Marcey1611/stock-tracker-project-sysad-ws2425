1806e0fa483be31616b6589d0e5520a649bdb305
np-223996 <np-223996@rwu.de>
2025-02-19 10:50:06 +0100

    #SYSAD-83 changed ports of stocktrackerapp 2

--- source/stock_tracker/lib/api_config.dart
+++ source/stock_tracker/lib/api_config.dart
@@ -4,10 +4,10 @@ class ApiConfig {
   static String getBaseUrl() {
     if (kIsWeb) {
       // for web
-      return 'http://localhost:8081/api/update_app';
+      return 'http://192.168.1.192:40105/api/update_app';
     } else {
       // for the Emulator
-      return 'http://10.0.2.2:8081/api/update_app';
+      return 'http://10.0.2.2:40105/api/update_app';
     }
   }
 }
