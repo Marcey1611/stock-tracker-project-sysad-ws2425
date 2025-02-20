140d0d14f6bf79015285b4de5b06dd8475b7a614
np-223996 <np-223996@rwu.de>
2025-02-20 16:16:22 +0100

    #SYSAD-53 init Klassen diagramm

--- source/detectionService/app/service/detection/frame_codings.py
+++ source/detectionService/app/service/detection/frame_codings.py
@@ -1,12 +1,16 @@
 import base64
 import cv2
+import numpy as np
 
 
-def encode_frame(frame ):
+def encode_frame(frame):
     success, buffer = cv2.imencode('.webp', frame, [cv2.IMWRITE_WEBP_QUALITY, 90])
-
     if success:
         base64_encoded_image = base64.b64encode(buffer)
         return base64_encoded_image
     else:
         return None
+
+def decode_frame(frame_bytes):
+    np_arr = np.frombuffer(frame_bytes, np.uint8)
+    return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
--- source/detectionService/app/service/detection/frame_detecting.py
+++ source/detectionService/app/service/detection/frame_detecting.py
@@ -7,8 +7,6 @@ from entities.detection.track_manager import TrackerManager
 from service.tracking.tracking_service import handle_disappeared_objects, update_database, update_object_tracking
 
 logger = logging.getLogger(__name__)
-TOLERANCE = 10
-ADD_REMOVE_THRESHOLD = 10
 
 file_location = "../../."+os.getenv('DETECTION_MODEL')
 device = os.getenv('DEVICE_TO_RUN_MODEL')
@@ -16,7 +14,7 @@ device = os.getenv('DEVICE_TO_RUN_MODEL')
 model = YOLO(file_location).to(device)
 model_cls_names = model.names
 
-def process_frame(frame,trackers:TrackerManager):
+def frame_detection(frame, trackers:TrackerManager):
     results = model.track(frame, conf=0.55, imgsz=640, verbose=False,persist=True)
     annotated_frame = results[0].plot()
 
