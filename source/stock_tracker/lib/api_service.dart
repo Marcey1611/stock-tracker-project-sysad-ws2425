fdb8bd0b7fe09899d60596f72de100ae3c6d7319
me-223965 <me-223965@rwu.de>
2025-02-20 12:32:09 +0100

    SYSAD83: Made some changes to test the (Android) App

--- source/stock_tracker/lib/api_service.dart
+++ source/stock_tracker/lib/api_service.dart
@@ -1,14 +1,11 @@
-import 'package:logger/logger.dart';
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'api_config.dart';
-final Logger logger = Logger();
+
 Future<List<dynamic>> getDataFromApi() async {
   final url = ApiConfig.getBaseUrl();
   try {
-    final response = await http.get(Uri.parse(url));
-    logger.i("${Uri.parse(url)}")
+    final response = await http.get(Uri.parse(url))
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       return data['products'] ?? []; 
